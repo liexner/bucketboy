1a6256a86242f1029cd0f36c5393f61b96843d72
Linus Exner Ådemark <linus.exnerademark@combitech.com>
2026-06-10 08:55:21 +0200

    added a oidc_enable env var

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,10 +1,12 @@
 import { redirect, type Handle } from "@sveltejs/kit"
 import { handle as authHandle } from "./auth"
 import { sequence } from "@sveltejs/kit/hooks"
+import { env } from "$env/dynamic/private"
 
 const publicPaths = ["/auth", "/login", "/logout"]
 
 const authorizationHandle: Handle = async ({ event, resolve }) => {
+	if (env.OIDC_ENABLE !== "true") return resolve(event)
 	const isPublic = publicPaths.some((p) => event.url.pathname.startsWith(p))
 	if (!isPublic) {
 		const session = await event.locals.auth()
