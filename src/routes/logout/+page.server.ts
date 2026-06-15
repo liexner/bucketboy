53453eb7f185ba821d2b127f76a57a68e494371a
Linus Exner Ådemark <linus.exnerademark@combitech.com>
2026-06-15 15:39:09 +0200

    made logout url more dynamic

--- src/routes/logout/+page.server.ts
+++ src/routes/logout/+page.server.ts
@@ -1,10 +1,17 @@
-import { redirect } from "@sveltejs/kit"
-import { env } from "$env/dynamic/private"
-import type { PageServerLoad } from "./$types"
+import { redirect } from '@sveltejs/kit';
+import { env } from '$env/dynamic/private';
+import type { PageServerLoad } from './$types';
+import { error } from 'console';
 
 export const load: PageServerLoad = async (event) => {
-	const url = new URL(`${env.OIDC_ISSUER}/protocol/openid-connect/logout`)
-	url.searchParams.set("client_id", env.OIDC_CLIENT_ID)
-	url.searchParams.set("post_logout_redirect_uri", event.url.origin)
-	redirect(303, url.toString())
-}
+	const response = await fetch(`${env.OIDC_ISSUER}/.well-known/openid-configuration`);
+	if (!response.ok) error(502, 'Failed to fetch OIDC discovery document');
+
+	const { end_session_endpoint } = await response.json();
+	if (!end_session_endpoint) redirect(303, '/');
+
+	const url = new URL(end_session_endpoint);
+	url.searchParams.set('client_id', env.OIDC_CLIENT_ID);
+	url.searchParams.set('post_logout_redirect_uri', event.url.origin);
+	redirect(303, url.toString());
+};
